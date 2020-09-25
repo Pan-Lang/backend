f29b48ae3a7bf6f9a4491667e9e0628c63147b10
QwertycowMoo <qwertycowmoo@gmail.com>
2020-09-24 20:36:02 -0500

    starting with an easy help route

--- app/index.js
+++ app/index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT||3000
 
+const uri = "mongodb+srv://QwertycowMoo:2Deb9281a1asdf@panlang-cluster.ipmwv.mongodb.net/<dbname>?retryWrites=true&w=majority"
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.get('/help', (req, res, next) => {
+  res.send('This is to help you');
 })
