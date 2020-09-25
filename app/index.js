ac168ad5dc94a89ccd3e6364fc000576c39b4b78
kgs5 <kgs5@illinois.edu>
2020-09-24 22:34:45 -0500

    Added associated endpoints and comments.

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const MongoClient = require('mongodb').MongoClient
 const app = express()
 const port = process.env.PORT||3000
-
+const router = express.Router();
 
 
 const uri = "mongodb+srv://QwertycowMoo:2Deb9281a1asdf@panlang-cluster.ipmwv.mongodb.net/mckinley-foundation?retryWrites=true&w=majority"
@@ -22,18 +22,38 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-app.get('/help', (req, res) => {
-  res.send('This is to help you');
+// *********** Person Endpoints *********** //
+
+// Gets all of the person records
+app.get('/person', (req, res) => {
+  // code here
 })
 
+// Creates a new person record in the format of our JSON schema
+app.post('/person', (req, res) => {
+  // code here
+})
+
+
+// *********** Stock Endpoints *********** //
+
+// Updates the amount of stock 
+// Request body example: { "newCount" : 12}
 app.put('/stock', (req, res) => {
   getPeopleCollection().find().then(result => {
     console.log(result)
   })
 })
 
+// Gets all of the stock as a JSON object
 app.get('/stock', (req, res) => {
   getStockCollection().then(result => {
     console.log(result)
   })
+})
+
+// Creates a new stock item
+// Request body example : {"name" : "Lays potato chips", "count" : 20}
+app.post('/stock', (req, res) => {
+  // code here
 })
