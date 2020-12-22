5a0836195373c468a67113d1cd01e3f362208192
QwertycowMoo <qwertycowmoo@gmail.com>
2020-12-21 20:40:33 -0500

    beginning the firestore learning and how to host things

---
+++ functions/index.js
@@ -0,0 +1,31 @@
+const functions = require('firebase-functions');
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+//this is an HTTP endpoint, but does not have a specific request, GET POST REPLACE etc
+//Is also synchronous
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+    //get text param
+    //so the kind of HTTP request would be under req.method
+    //but we can create other exports._________ the same way we make endpoints I think
+    const original = req.query.text;
+
+    const writeResult = await admin.firestore().collection('messages').add({original: original});
+    res.json({result: `Message with ID: ${writeResult.id} added.`});
+})
+
+exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
+    .onCreate((snapshot, context) => {
+        //grab the current value of thing written to Firestore
+        const original = snapshot.data().original;
+
+        //Access the parameter {documentId} with context.params
+        //Logs this to the log with severity "INFO"
+        functions.logger.log('Uppercasing', context.params.documentId, original);
+
+        const uppercase = original.toUpperCase();
+        //return a Promise because asynchronous
+        //writes to Firestore
+        return snapshot.ref.set({uppercase}, {merge: true});
+    })
